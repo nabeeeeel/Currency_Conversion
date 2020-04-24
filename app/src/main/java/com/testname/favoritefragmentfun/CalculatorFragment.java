07ee2d8fe3386419e72c6a88c02b473125b5086b
nabeeeeel <nsahmed@protonmail.com>
2020-04-23 20:06:38 -0400

    Calculator Working

--- app/src/main/java/com/testname/favoritefragmentfun/CalculatorFragment.java
+++ app/src/main/java/com/testname/favoritefragmentfun/CalculatorFragment.java
@@ -1,6 +1,7 @@
 package com.testname.favoritefragmentfun;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,133 +28,157 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.lang.reflect.Array;
 import java.util.Locale;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class CalculatorFragment extends Fragment {
 
-    private Spinner FirstSpinner, SecondSpinner, OperationsSpinner;
-    private EditText FirstText, SecondText;
-    private TextView ResultsText;
-    private String FirstType = "Select initial currency…",
-            SecondType = "Select desired currency…",
-            OperationsType = "Select desired operation…";
-
+    private Spinner initialSpinner,
+            targetSpinner;
+    Spinner symbolSpinner;
+    private String initialType = "Select initial currency…",
+            targetType = "Select desired currency…",
+            symbol = "+";
+    private EditText op1Text, op2Text;
+    private TextView text;
     private float result;
-    private RequestQueue requestQueue = null;
-
     private static final float EURO = 1.0f;
 
+    private static final String TARGET_SPINNER2 = "RESULT_T2";
+    private static final String INPUT_SPINNER2 = "INPUT_T2";
+    private static final String INPUT2 = "INPUT2";
+    private static final String INPUT22 = "INPUT22";
+    private static final String TEXT2 = "TEXT2";
+    private static final String SYMBOL_T2 = "SYMBOL_T2";
+    private Context mContext;
+
+    private RequestQueue requestQueue = null;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
         SharedPreferences settings = getActivity().getPreferences(MODE_PRIVATE);
 
-        View view = inflater.inflate(R.layout.calculator_fragment, container, false);
+        View view = inflater.inflate(R.layout.calculator_fragment,
+                container, false);
 
-        //set the first spinner
-        FirstSpinner = view.findViewById(R.id.firstSpinner);
-        //create adapter with spinner items
-        final ArrayAdapter<String> firstAdapter = new ArrayAdapter<String>(
+
+        // set up initial spinner
+        initialSpinner = view.findViewById(R.id.initialSpinner);
+        // create adapter with spinner items
+        final ArrayAdapter<String> initialAdapter = new ArrayAdapter<String>(
                 view.getContext(),
                 R.layout.spinner_resource,
                 getResources().getStringArray(R.array.conversionsFrom)
         );
-        firstAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        FirstSpinner.setAdapter(firstAdapter);
-        //spinner event
-        FirstSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        initialAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        initialSpinner.setAdapter(initialAdapter);
+        // spinner event
+        initialSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                FirstType = FirstSpinner.getSelectedItem().toString();
+                initialType = initialSpinner.getSelectedItem().toString();
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onNothingSelected(AdapterView<?> parent) {}
 
-            }
         });
 
-        //set the second spinner
-        SecondSpinner = view.findViewById(R.id.secondSpinner);
+        // set up target spinner
+        targetSpinner = view.findViewById(R.id.targetSpinner);
         // create adapter with spinner items
-        final ArrayAdapter<String> secondAdapter = new ArrayAdapter<String>(
+        final ArrayAdapter<String> targetAdapter = new ArrayAdapter<String>(
                 view.getContext(),
                 R.layout.spinner_resource,
                 getResources().getStringArray(R.array.conversionsTo)
         );
-        secondAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        SecondSpinner.setAdapter(secondAdapter);
-        //spinner event
-        SecondSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        targetAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        targetSpinner.setAdapter(targetAdapter);
+        // spinner event
+        targetSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                SecondType = SecondSpinner.getSelectedItem().toString();
+                targetType = targetSpinner.getSelectedItem().toString();
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onNothingSelected(AdapterView<?> parent) {}
 
-            }
         });
 
-        //set the operations spinner
-        OperationsSpinner = view.findViewById(R.id.operationsSpinner);
-        //create adapter with spinner items
-        final ArrayAdapter<String> operationsAdapter = new ArrayAdapter<String>(
+        symbolSpinner = (Spinner) view.findViewById(R.id.spinner);
+
+        final ArrayAdapter<String> symbolAdapter = new ArrayAdapter<String>(
                 view.getContext(),
                 R.layout.spinner_resource,
                 getResources().getStringArray(R.array.operations)
         );
-        operationsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        OperationsSpinner.setAdapter(secondAdapter);
-        //spinner event
-        OperationsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        symbolAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        symbolSpinner.setAdapter(symbolAdapter);
+        symbolSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                OperationsType = OperationsSpinner.getSelectedItem().toString();
+                symbol = symbolSpinner.getSelectedItem().toString();
             }
 
             @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+            public void onNothingSelected(AdapterView<?> parent) {}
 
-            }
         });
 
-        ResultsText = view.findViewById(R.id.ResultsText);
-        FirstText = view.findViewById(R.id.FirstNumberText);
-        SecondText = view.findViewById(R.id.SecondNumberText);
-        Button calculateButton = view.findViewById(R.id.calculateButton);
+        text = view.findViewById(R.id.textView);
+        op1Text = view.findViewById(R.id.toConvert1Text);
+        op2Text = view.findViewById(R.id.toConvert2Text);
 
-        calculateButton.setOnClickListener(new View.OnClickListener() {
-                @SuppressLint("DefaultLocale")
-                @Override
-                public void onClick(View v) {
-                    if (FirstType.equals("Select initial currency…") || SecondType.equals("Select desired currency…")) {
-                        Toast toast = Toast.makeText(getActivity(), "Select initial and desired currency…", Toast.LENGTH_SHORT);
+        Button mainButton = view.findViewById(R.id.button);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void onClick(View v) {
+                if (initialType.equals("Select initial currency…") || targetType.equals("Select desired currency…")) {
+                    Toast toast = Toast.makeText(getActivity(), "Select initial and desired currency…", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    if ((op1Text.getText().toString()).equals("") || op2Text.getText().toString().equals("")) {
+                        Toast toast = Toast.makeText(getActivity(), "Input amounts convert…", Toast.LENGTH_SHORT);
                         toast.show();
-                    }else if (OperationsType.equals("Select desired operation…")){
-                        Toast toast = Toast.makeText(getActivity(), "Select desired operation…", Toast.LENGTH_SHORT);
-                        toast.show();
-                    }else{
-                        if((FirstText.getText().toString()).equals("") || (SecondText.getText().toString()).equals("")){
-                            Toast toast = Toast.makeText(getActivity(), "Input amount to convert…", Toast.LENGTH_SHORT);
-                            toast.show();
-                        }else{//get number to convert
-                            calculate((FirstType.split(" "))[0], SecondType, Float.parseFloat(FirstText.getText().toString()), OperationsType, Float.parseFloat(SecondText.getText().toString()));
+                    } else {// get number to convert
+                        calculate((initialType.split(" "))[0],
+                                targetType,
+                                Float.parseFloat(op1Text.getText().toString()),
+                                Float.parseFloat(op2Text.getText().toString()));
+
                     }
-
                 }
             }
         });
 
 
+        int initialSpinnerPosition = initialAdapter.getPosition(settings.getString(INPUT_SPINNER2, initialType));
+        initialSpinner.setSelection(initialSpinnerPosition);
+
+        int targetSpinnerPosition = targetAdapter.getPosition(settings.getString(TARGET_SPINNER2, targetType));
+        targetSpinner.setSelection(targetSpinnerPosition);
+
+        int symbolSpinnerPosition = symbolAdapter.getPosition(settings.getString(SYMBOL_T2, symbol));
+        symbolSpinner.setSelection(symbolSpinnerPosition);
+
+        op1Text.setText(settings.getString(INPUT2,""));
+
+        text.setText(settings.getString(TEXT2, "Select a Currency to convert to and from"));
+
+        op2Text.setText(settings.getString(INPUT22, ""));
 
         return view;
+
     }//end onCreateView
 
-    private void calculate(final String from, final String to, final float startingNumber, final String operation, final float secondNumber) {
+    private void calculate(final String from, final String to, final float first, final float second) {
+
         String url = "http://data.fixer.io/api/latest?access_key="
                 +  getString(R.string.api_key)
                 +  "&symbols="
@@ -178,11 +203,14 @@ public class CalculatorFragment extends Fragment {
                             float baseInitial = Float.parseFloat(element.getString(from));
                             float baseResult = Float.parseFloat(element.getString(to));
 
-                            float baseInitialConversionCoefficient = EURO / baseInitial;
-                            result = startingNumber * baseInitialConversionCoefficient;
+                            float firstInitialConversionCoefficient = EURO / baseInitial;
+                            result = first * firstInitialConversionCoefficient;
                             result *= baseResult;
 
-                            ResultsText.setText(String.format(Locale.US, "%.2f %s", result, to));
+                            if (symbol.equals("+")) result += second;
+                            else result -= second;
+
+                            text.setText(String.format(Locale.US, "%.2f %s", result, to));
 
 
                         } catch (JSONException ex) {
@@ -201,6 +229,31 @@ public class CalculatorFragment extends Fragment {
                 }
         );
         requestQueue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString(INPUT_SPINNER2, initialType);
+        outState.putString(TARGET_SPINNER2, targetType);
+        outState.putString(INPUT2, op1Text.getText().toString());
+        outState.putString(INPUT22, op2Text.getText().toString());
+        outState.putString(TEXT2, text.getText().toString());
+        outState.putString(SYMBOL_T2, symbol);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        SharedPreferences settings = getActivity().getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(INPUT_SPINNER2, initialType);
+        editor.putString(TARGET_SPINNER2, targetType);
+        editor.putString(INPUT2, op1Text.getText().toString());
+        editor.putString(INPUT22, op2Text.getText().toString());
+        editor.putString(TEXT2, text.getText().toString());
+        editor.putString(SYMBOL_T2, symbol);
+        editor.apply();
     }
 }
 
